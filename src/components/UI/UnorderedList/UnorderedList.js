c27013700f08ba233400ef2bd056584d3e5746a4
Michael McDevitt <mmcdevi1@gmail.com>
2019-02-06 14:24:45 -0500

    Add ListItem component for dropdown list

--- src/components/UI/UnorderedList/UnorderedList.js
+++ src/components/UI/UnorderedList/UnorderedList.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import ListItem from "./ListItem";
 
 const UnorderedListWrapper = styled.ul`
   display: ${props => (props.show ? "block" : "none")};
 `;
 
 class UnorderedList extends Component {
+  renderList() {
+    const { data } = this.props;
+
+    return data.map(item => {
+      return <ListItem key={item.name}>{item.name}</ListItem>;
+    });
+  }
+
   render() {
-    const { show, data } = this.props;
+    const { show } = this.props;
 
     return (
       <UnorderedListWrapper show={show}>
-        {data.map(item => {
-          return <li key={item.name}>{item.name}</li>;
-        })}
+        {this.renderList()}
       </UnorderedListWrapper>
     );
   }
